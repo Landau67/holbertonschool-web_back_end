7cdb6e1eda4d4a17e7deffa6460f50cb4208814d
landau <landau@landau.landau>
2025-11-28 21:25:44 +0400

    ...

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""V<dwnufndefewfwd"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """fkomwomdfkdwfvwd"""
+    return list(mongo_collection.find({ "topics":topic }))
