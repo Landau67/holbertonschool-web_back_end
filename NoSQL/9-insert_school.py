840ef18aed124c531f807646d0e060a5a4d6cb57
landau <landau@landau.landau>
2025-11-28 21:08:04 +0400

    ...

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""fojwndvucnhdfsivm"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """fvwoifvmdwf,erwfv"""
+    result = mongo_collection.insert(kwargs)
+    return result.inserted_id
